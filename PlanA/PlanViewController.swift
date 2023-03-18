839e627c1c4e658a53cf53a48798fc65ae0d86b8
AidanPetr <50280191+AidanPetr@users.noreply.github.com>
2023-03-17 19:21:36 -0500

    added functionality to generatedPlan view

--- PlanA/PlanViewController.swift
+++ PlanA/PlanViewController.swift
@@ -9,13 +9,14 @@ import UIKit
 
 class PlanViewController: UIViewController {
 
+    @IBOutlet var navBar: UINavigationItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
     }
     
-
     /*
     // MARK: - Navigation
 
